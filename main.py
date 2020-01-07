3ab2c930fa6c36afd9df7d267e5629934bd5954c
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-07 16:43:02 +0300

    Add led blink code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from bluedot import BlueDot
-
+from gpiozero import LED
+from time import sleep
 bd = BlueDot()
 
+led = LED(17)
 
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
 
