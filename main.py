201607f8a5d243d914cff2bad423e13c239340b4
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-07 17:17:38 +0300

    Add print try code

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from bluedot import BlueDot
-from gpiozero import LED
-from time import sleep
-bd = BlueDot()
-
-led = LED(17)
-
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+print("Hey ben değiştim")
 
