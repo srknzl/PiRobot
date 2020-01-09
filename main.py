e4fb8fba0f8144e18298913da0660ed5a60caf86
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-09 22:30:35 +0300

    Add some more message types like turn left and adjust speed

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 import time
 from gpiozero import Motor
 import bluetooth
+from bluetooth import BluetoothSocket
 # from signal import pause
 
+PORT = 1
 server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-port = 1
-server_socket.bind(("", port))
+server_socket.bind(("", PORT))
 server_socket.listen(1)
-
 client_socket, address = server_socket.accept()
+client_socket = BluetoothSocket(client_socket)
 print("Accepted connection from ", address)
-while 1:
+
+
+leftMotor = Motor(23, 24, 18, pwm=True)  # 23-> pin16, 24->pin18, 18-> pin12
+rightMotor = Motor(27, 22, 19, pwm=True)  # 27-> pin13, 22-> pin15, 19-> pin35
+
+while True:
     data = client_socket.recv(1024).decode("utf-8")
+    data = data.rstrip('\r\n')
 
-    if data == "run\r\n":
-        leftMotor = Motor(23, 24, 18, pwm=True)  # 23-> pin16, 24->pin18, 18-> pin12
-        rightMotor = Motor(27, 22, 19, pwm=True)  # 27-> pin13, 22-> pin15, 19-> pin35
-
-        leftMotor.forward(0.5)
-        rightMotor.forward(0.5)
-        time.sleep(1)
-        leftMotor.backward(1)
-        rightMotor.backward(1)
-        time.sleep(1)
-    else:
-        print(data)
-
+    splittedData = data.split("|")
+    message = splittedData[0]
+    if message == "left":
+        leftMotor.forward(0.3)
+        rightMotor.forward(0.7)
+        # todo Add left led code
+    elif message == "right":
+        leftMotor.forward(0.7)
+        rightMotor.forward(0.3)
+        # todo Add right led code
+    elif message == "left90":
+        pass # todo Add 90 degree left turn code
+    elif message == "right90":
+        pass # todo Add 90 degree left turn code
+    elif message == "speed":
+        if len(splittedData) != 2:
+            client_socket.send("Wrong usage of speed command" + str(splittedData))
+            print("Wrong usage of speed command", splittedData)
+            continue
+        leftMotor.value = splittedData[1]
+        pass # todo Add 90 degree left turn code
+    #print(data)
