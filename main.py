63f837d03d9f8ad237766ad2d2dc086727d07d02
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-09 21:57:39 +0300

    Change dir of second motor to make both same direction

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gpiozero import Motor
 from signal import pause
 
 leftMotor = Motor(23, 24, 18, pwm=True)  # 23-> pin16, 24->pin18, 18-> pin12
-rightMotor = Motor(22, 27, 19, pwm=True)  # 22-> pin15, 27-> pin13, 19-> pin35
+rightMotor = Motor(27, 22, 19, pwm=True)  # 27-> pin13, 22-> pin15, 19-> pin35
 
 while True:
     leftMotor.forward(0.5)
