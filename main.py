0a360f90e0494cfe6bf3ed0232ca08f156bbfd7d
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-09 21:56:33 +0300

    Add second motor

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
 from gpiozero import Motor
 from signal import pause
-motor = Motor(23, 24, 18, pwm=True) #23-> pin16, 24->pin18, 18-> pin12
+
+leftMotor = Motor(23, 24, 18, pwm=True)  # 23-> pin16, 24->pin18, 18-> pin12
+rightMotor = Motor(22, 27, 19, pwm=True)  # 22-> pin15, 27-> pin13, 19-> pin35
 
 while True:
-        motor.forward(0.5)
-        time.sleep(1)
-        motor.backward(1)
-        time.sleep(1)
+    leftMotor.forward(0.5)
+    rightMotor.forward(0.5)
+    time.sleep(1)
+    leftMotor.backward(1)
+    rightMotor.backward(1)
+    time.sleep(1)
 pause()
-
