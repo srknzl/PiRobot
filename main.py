4056f992ecb295983320165738452a1c557b9d66
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-09 21:50:34 +0300

    Add first working motor code

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from gpiozero import Motor, PWMOutputDevice
+import time
+from gpiozero import Motor
 from signal import pause
-pwm = PWMOutputDevice("GPIO26")
-motor = Motor(4, 5)
-motor.forward()
-pwm.value = 0.5
+motor = Motor(23, 24, 18, pwm=True) #23-> pin16, 24->pin18, 18-> pin12
 
+while True:
+        motor.forward(0.5)
+        time.sleep(1)
+        motor.backward(1)
+        time.sleep(1)
 pause()
+
