19309f8c3725561f16a7a5f3b7d2a9a3de8b88ab
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-17 00:42:12 +0300

    Change buzzer pin so that it does not beep when not running

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ connect()
 leftMotor = Motor(23, 24, 18, pwm=True)  # In1 23-> pin16, In2 24->pin18, 18-> pin12
 rightMotor = Motor(27, 22, 19, pwm=True)  # 27-> pin13, 22-> pin15, 19-> pin35
 
-buzzer = Buzzer(3)
+buzzer = Buzzer(17)
 beep()
 speedSensorCounter = 0
 speedSensor = Button(17)
