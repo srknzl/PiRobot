de590d68937bf9ba4e16ace6ac348329cb71ca7e
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-18 02:09:46 +0300

    Change joystick behaviour

--- main.py
+++ main.py
@@ -142,34 +142,34 @@ def listenForMessages(cs):
             left = 0
             right = 0
 
-            if 20 >= angle >= 0: # Turn right fast
+            if 40 >= angle >= 0: # Turn right fast
                 left = 1
                 right = 0.3
-            elif 70 >= angle > 20: # Turn right slowly
+            elif 70 >= angle > 40: # Turn right slowly
                 left = 1
                 right = 0.6
             elif 110 >= angle > 70: # Go forward
                 left = 1
                 right = 1
-            elif 160 >= angle > 110: # Turn left slowly
+            elif 140 >= angle > 110: # Turn left slowly
                 left = 0.6
                 right = 1
-            elif 180 >= angle > 160: # Turn left fast
+            elif 180 >= angle > 140: # Turn left fast
                 left = 0.3
                 right = 1
-            elif 200 >= angle > 180: # Turn back left fast
+            elif 220 >= angle > 180: # Turn back left fast
                 left = -0.3
                 right = -1
-            elif 250 >= angle > 200: # Turn back left slowly
+            elif 250 >= angle > 220: # Turn back left slowly
                 left = -0.6
                 right = -1
             elif 290 >= angle > 250: # Go back
                 left = -1
                 right = -1
-            elif 340 >= angle > 290: # Turn back right slowly
+            elif 320 >= angle > 290: # Turn back right slowly
                 left = -1
                 right = -0.6
-            elif angle > 340: # Turn back right fast
+            elif angle > 320: # Turn back right fast
                 left = -1
                 right = -0.3
 
